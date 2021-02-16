c5c105ccea7d8e6d96117c65887efffe1903fc12
Stefan Strandberg <stefan.mortensen@bonniernews.se>
2021-02-16 21:33:13 +0100

    Add sounds and hum when lightsaber is on

--- Lightsaber/Lightsaber.ino
+++ Lightsaber/Lightsaber.ino
@@ -11,13 +11,14 @@
  *   - Unique sounds
  *   - Blade color
  */
-#include <SPI.h>
+
 #include <SdFat.h>
 #include <TMRpcm.h>
 //s#include "I2Cdev.h"
 #include <ezButton.h>
 #include "FastLED.h"
 #include "MPU6050.h"
+#include <toneAC.h> 
 
 #ifdef __arm__
 // should use uinstd.h to define sbrk but Due causes a conflict
@@ -43,9 +44,16 @@ extern char *__brkval;
 #define PULSE_ALLOW 1       // blade pulsation (1 - allow, 0 - disallow)
 #define PULSE_AMPL 20       // pulse amplitude
 #define PULSE_DELAY 30      // delay between pulses
-#define VOLUME 6           // Maximum volume (should probably not be changed if volume is controlled with potentiometer)
+#define VOLUME 5           // Maximum volume (should probably not be changed if volume is controlled with potentiometer)
 #define INTERRUPT_PIN 2
 #define SD_ChipSelectPin 3
+
+#define SWING_TIMEOUT 500   // timeout between swings
+#define SWING_L_THR 150     // swing angle speed threshold
+#define SWING_THR 300       // fast swing angle speed threshold
+#define STRIKE_THR 150      // hit acceleration threshold
+#define STRIKE_S_THR 320    // hard hit acceleration threshold
+#define FLASH_DELAY 80      // flash time while hit
 // ---------------------------- VARIABLES -------------------------------
 
 CRGB pixels[BLADE_LEDS];    // Holds blade neopixels
@@ -62,13 +70,24 @@ ezButton soundButton(SOUND_PIN);  // Button to make a random sound from the curr
 
 byte red, green, blue, redOffset, greenOffset, blueOffset; // RGB color information and offsets
 unsigned long PULSE_timer; // Time since last pulse
+unsigned long bzzTimer;
 int PULSEOffset;
-
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+int gyroX, gyroY, gyroZ, accelX, accelY, accelZ, freq, freq_f = 20;
+unsigned long ACC, GYR, COMPL;
+unsigned long swing_timer, swing_timeout;
 float k = 0.2; 
 bool bladeActive; 
 bool firstLoop = true;
-
+boolean swing_flag, swing_allow, strike_flag;
+unsigned long humTimer = -9000, mpuTimer;
+byte nowNumber;
 // --------------------------------- SOUNDS ----------------------------------
+
+const char* humSound = "HUM.wav";
+const char* onSound = "ON.wav";
+const char* offSound = "OFF.wav";
 const char strike1[] PROGMEM = "SK1.wav";
 const char strike2[] PROGMEM = "SK2.wav";
 const char strike3[] PROGMEM = "SK3.wav";
@@ -140,17 +159,17 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  Serial.println("Initializing SD card...");
+  Serial.println(F("Initializing SD card..."));
   if (!SD.begin(3)) {
     //TODO: Play sound to indicate failure
-    Serial.println("initialization failed!");
+    Serial.println(F("initialization failed!"));
     while (1);
   }
-  Serial.println("SD card initialized.");
+  Serial.println(F("SD card initialized."));
   
   tmrpcm.speakerPin = SPEAKER_PIN;
   tmrpcm.setVolume(VOLUME);
-  tmrpcm.quality(1);
+  tmrpcm.quality(0);
 
   bladeButton.setDebounceTime(50);
   characterButton.setDebounceTime(50);
@@ -191,14 +210,15 @@ void setColor(byte color){
 
 void loop() {
   
-  bladePulse();
+ // bladePulse();
+  humTick();
   //getFreq();
   checkInput();
-  //tick();
+  //swingTick();
   //strike();
   //swing();
-  Serial.println(freeMemory());
-  delay(20);
+ // Serial.println(freeMemory());
+  //delay(20);
   
 }
 
@@ -213,7 +233,31 @@ void bladePulse(){
     }
 }
 
-void setPixel(int Pixel, byte red, byte green, byte blue) {
+void humTick() {
+    Serial.println(millis() - humTimer);
+    // Hum sound mode
+    if (((millis() - humTimer) > 9000) && bladeActive) {
+      Serial.println(F("HUMMMM!"));
+      tmrpcm.play("hum2.wav");
+      humTimer = millis();
+      swing_flag = 1;
+      strike_flag = 0;
+    }
+   
+/*
+    // Generate hum
+    long delta = millis() - bzzTimer;
+    if ((delta > 3) && bladeActive ) {
+      if (strike_flag) {
+        tmrpcm.disable();
+        strike_flag = 0;
+      }
+      toneAC(freq_f);
+      bzzTimer = millis();
+    } */
+}
+
+void setPixel(short Pixel, byte red, byte green, byte blue) {
   pixels[Pixel].r = red;
   pixels[Pixel].g = green;
   pixels[Pixel].b = blue;
@@ -235,7 +279,8 @@ void checkInput() {
   if (!firstLoop){ // Hack that skips first loop
     if(bladeButton.isPressed()){
       if (bladeActive != true) {
-         animateExtend();
+
+          animateExtend();
           digitalWrite(BLADE_BUTTON_LED_PIN, HIGH);
        
         }
@@ -247,9 +292,12 @@ void checkInput() {
 
     if(characterButton.isPressed()){
       // Character button
+              tmrpcm.play("HUM.wav");
+        delay(200);
     }
 
     if(soundButton.isPressed()){   
+      tmrpcm.play("hum2.wav");
       //Sound button
     }
   }
@@ -258,7 +306,7 @@ void checkInput() {
 
 
 void animateExtend() {
-  tmrpcm.play("ON.wav");
+  tmrpcm.play(onSound);
   for(int dot = 0; dot <= (BLADE_LEDS / 2 - 1); dot++) { 
       pixels[dot] = CRGB::Red;
       pixels[BLADE_LEDS - 1 - dot] = CRGB::Red;
@@ -270,9 +318,8 @@ void animateExtend() {
   
 
 void animateRetract() {
-  tmrpcm.play("OFF.wav");
+  tmrpcm.play(offSound);
   for(int dot = (BLADE_LEDS / 2 - 1); dot >= 0; dot--) { 
-     Serial.println(dot);
       pixels[dot] = CRGB::Black;
       pixels[BLADE_LEDS - 1 - dot] = CRGB::Black;
       delay(10);
@@ -281,7 +328,103 @@ void animateRetract() {
   bladeActive = false;
 }
 
+void getFreq() {
+  // 100% borrowed! Credits to MadGyver https://github.com/AlexGyver/EnglishProjects/blob/master/GyverSaber/GyverSaber/GyverSaber.ino
+  if (bladeActive) {
+    if (millis() - mpuTimer > 500) {                            
+      accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);       
 
+      // find absolute and divide on 100
+      gyroX = abs(gx / 100);
+      gyroY = abs(gy / 100);
+      gyroZ = abs(gz / 100);
+      accelX = abs(ax / 100);
+      accelY = abs(ay / 100);
+      accelZ = abs(az / 100);
+
+      // vector sum
+      ACC = sq((long)accelX) + sq((long)accelY) + sq((long)accelZ);
+      ACC = sqrt(ACC);
+      GYR = sq((long)gyroX) + sq((long)gyroY) + sq((long)gyroZ);
+      GYR = sqrt((long)GYR);
+      COMPL = ACC + GYR;
+      /*
+         Serial.print("$");
+         Serial.print(gyroX);
+         Serial.print(" ");
+         Serial.print(gyroY);
+         Serial.print(" ");
+         Serial.print(gyroZ);
+         Serial.println(";");
+      */
+      freq = (long)COMPL * COMPL / 1500;                        // parabolic tone change
+      freq = constrain(freq, 18, 300);                          
+      freq_f = freq * k + freq_f * (1 - k);                     // smooth filter
+      mpuTimer = micros();                                     
+    }
+  }
+}
+
+void swingTick() {
+  if (GYR > 80 && (millis() - swing_timeout > 100)) {
+    swing_timeout = millis();
+    if (((millis() - swing_timer) > SWING_TIMEOUT) && swing_flag && !strike_flag) {
+      if (GYR >= SWING_THR) {      
+        nowNumber = random(5);          
+        strcpy_P(BUFFER, (char*)pgm_read_word(&(swings[nowNumber])));
+        tmrpcm.play(BUFFER);
+        humTimer = millis() - 9000 + swing_time[nowNumber];
+        swing_flag = 0;
+        swing_timer = millis();
+        swing_allow = 0;
+      }
+      if ((GYR > SWING_L_THR) && (GYR < SWING_THR)) {
+        nowNumber = random(5);            
+        strcpy_P(BUFFER, (char*)pgm_read_word(&(swings_L[nowNumber])));
+        tmrpcm.play(BUFFER);              
+        humTimer = millis() - 9000 + swing_time_L[nowNumber];
+        swing_flag = 0;
+        swing_timer = millis();
+        swing_allow = 0;
+      }
+    }
+  }
+}
+
+void strikeTick() {
+  if ((ACC > STRIKE_THR) && (ACC < STRIKE_S_THR)) {
+    //if (!HUMmode) noToneAC();
+    nowNumber = random(8);
+    // читаем название трека из PROGMEM
+    strcpy_P(BUFFER, (char*)pgm_read_word(&(strikes_short[nowNumber])));
+    tmrpcm.play(BUFFER);
+    hit_flash();
+//    if (!HUMmode)
+//      bzzTimer = millis() + strike_s_time[nowNumber] - FLASH_DELAY;
+//    else
+      humTimer = millis() - 9000 + strike_s_time[nowNumber] - FLASH_DELAY;
+    strike_flag = 1;
+  }
+  if (ACC >= STRIKE_S_THR) {
+    //if (!HUMmode) noToneAC();
+    nowNumber = random(8);
+    // читаем название трека из PROGMEM
+    strcpy_P(BUFFER, (char*)pgm_read_word(&(strikes[nowNumber])));
+    tmrpcm.play(BUFFER);
+    hit_flash();
+    //if (!HUMmode)
+    //  bzzTimer = millis() + strike_time[nowNumber] - FLASH_DELAY;
+    //else
+      humTimer = millis() - 9000 + strike_time[nowNumber] - FLASH_DELAY;
+    strike_flag = 1;
+  }
+}
+
+void hit_flash() {
+  setAllBladePixels(255, 255, 255);            
+  delay(FLASH_DELAY);                
+  setAllBladePixels(red, blue, green);        
+}
 
 int freeMemory() {
   char top;
